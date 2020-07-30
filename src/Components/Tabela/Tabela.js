a0e07720a5349c3a941a75084d6ef2e0565c390e
Luis Henrique <lhzimmermann@gmail.com>
2020-07-30 10:15:55 -0300

    Commit - Parte 3 - Aula 1
    MaterialUI

--- src/Components/Tabela/Tabela.js
+++ src/Components/Tabela/Tabela.js
@@ -1,45 +1,40 @@
-import React, {Component} from 'react';
+  
+import React, { Component } from 'react'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Button from '@material-ui/core/Button'
 
-const TableHead = () => {
-  return (
-  <thead>
-      <tr>
-          <th>Autores</th>
-          <th>Livros</th>
-          <th>Preços</th>
-          <th>Remover</th>
-      </tr>
-  </thead>
-  );
-}
 
-const TableBody = props => {
-  const linhas = props.autores.map((linha, index) => {
-    return (
-      <tr key={linha.id}>
-        <td>{linha.nome}</td>
-        <td>{linha.livro}</td>
-        <td>{linha.preco}</td>
-        <td><button className="waves-effect waves-light indigo lighten-2 btn" onClick = {() => {props.removeAutor(linha.id) }}>Remover</button></td>
-      </tr>
-    )
-  });
-
-  return(
-    <tbody>
-      {linhas}
-    </tbody>
-  );
-}
 
 class Tabela extends Component {
   render() {
     const { autores, removeAutor } = this.props;
     return(
-      <table className="centered highlight">
-          <TableHead />
-          <TableBody autores = { autores } removeAutor = {removeAutor} />
-      </table>
+      <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Autores</TableCell>
+              <TableCell>Livros</TableCell>
+              <TableCell>Preços</TableCell>
+              <TableCell>Remover</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {autores.map(autor => 
+             (
+              <TableRow key={autor.id}>
+                <TableCell>{autor.nome}</TableCell>
+                <TableCell>{autor.livro}</TableCell>
+                <TableCell>{autor.preco}</TableCell>
+                <TableCell><Button variant="contained" color="primary" onClick = {() => {removeAutor(autor.id) }}>Remover</Button></TableCell>
+              </TableRow>
+            )
+          )}
+          </TableBody>
+      </Table>
     );
   }
 }
