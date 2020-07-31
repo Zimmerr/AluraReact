49ba4d5560cf2529822d0bed7da916d65a91fb54
Luis Henrique <lhzimmermann@gmail.com>
2020-07-31 10:55:11 -0300

    Commit - Parte 3 - Aula 4
    Forms com MaterialUI, SnackBar

--- src/Components/Formulario/Formulario.js
+++ src/Components/Formulario/Formulario.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
+import TextField from '@material-ui/core/TextField'
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+
+
 import FormValidator from '../../utils/FormValidator'
 import PopUp from '../../utils/PopUp'
+import Toast from '../Toast/Toast'
 
 class Formulario extends Component {
   constructor(props){
@@ -32,6 +38,8 @@ class Formulario extends Component {
       nome:'',
       livro:'',
       preco:'',
+      validacao:this.validador,
+      open: true
     }
 
     this.state = this.stateInicial;
@@ -69,47 +77,51 @@ class Formulario extends Component {
     const {nome, livro, preco} = this.state;
 
     return (
-      <form>
-        <div className="row">
-          <div className="input-field col s4">
-            <label className="input-field" htmlFor="nome">Nome</label>
-            <input
-              className="validate"
-              id="nome"
-              type="text"
-              name="nome"
-              value={nome}
-              onChange={this.escutadorDeInput}
-            />
-          </div>
-          <div className="input-field col s4">
-            <label className="input-field" htmlFor="livro">Livro</label>
-            <input
-              className="validate"
-              id="livro"
-              type="text"
-              name="livro"
-              value={livro}
-              onChange={this.escutadorDeInput}
-            />
-
-          </div>
-          <div className="input-field col s4">
-            <label className="input-field" htmlFor="preco">Preço</label>
-            <input
-              className="validate"
-              id="preco"
-              type="text"
-              name="preco"
-              value={preco}
-              onChange={this.escutadorDeInput}
-            />
-          </div>
-        </div>
-          <button className="waves-effect waves-light indigo lighten-2 btn" onClick={this.submitFormulario} type="button">Salvar
-          </button>
-      </form>
-
+      <>
+        <Toast open={this.state.open} handleClose={() => this.setState({open: false})}>
+          Toast Funcionando
+        </Toast>
+        <form>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item>
+              <TextField 
+              id="nome" 
+              name="nome" 
+              label="Nome" 
+              variant="outlined" 
+              value={nome} 
+              onChange={this.escutadorDeInput}/>
+            </Grid>
+            <Grid item>
+            <TextField 
+              id="livro" 
+              name="livro" 
+              label="Livro" 
+              variant="outlined" 
+              value={livro} 
+              onChange={this.escutadorDeInput}/>
+            </Grid>
+            <Grid item>
+              <TextField 
+                id="preco" 
+                name="preco" 
+                label="Preço" 
+                variant="outlined" 
+                value={preco} 
+                onChange={this.escutadorDeInput}/>
+            </Grid>
+            <Grid item>
+              <Button 
+                variant="contained"
+                color="primary" 
+                onClick={this.submitFormulario} 
+                type="Button">Salvar
+              </Button>
+            </Grid>
+          </Grid>
+            
+        </form>
+      </>
   )
   }
 }
---
+++ src/Components/Toast/Toast.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SnackBar from '@material-ui/core/SnackBar'
+import Alert from '@material-ui/lab/Alert'
+
+export default ({open, handleClose, children, severity}) => (
+  <SnackBar 
+          open={open} 
+          onClose= {handleClose}
+          autoHide={2000}
+        >
+        <Alert onClose= {handleClose} variant='filled' severity={severity}>
+          {children}
+        </Alert>
+        
+        </SnackBar>
+)
